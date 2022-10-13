4713b1bf72c622fc0f67ccfafced802da53a505d
Brendo <brendo.coder@gmail.com>
2022-10-12 23:41:57 -0300

    Fixed macros

--- include/definitions.h
+++ include/definitions.h
@@ -3,12 +3,12 @@
 #define invalid_id -1
 
 namespace locale {
-#define account_already_exists(id) "ERRO: CONTA (id) JA EXISTENTE"
-#define account_does_not_exist(id) "ERRO: CONTA (id) NAO EXISTE"
-#define account_created(id) "OK: CONTA (id) CADASTRADA"
-#define account_removed(id) "OK: CONTA (id) REMOVIDA"
+#define account_already_exists(id) "ERRO: CONTA " << id << " JA EXISTENTE"
+#define account_does_not_exist(id) "ERRO: CONTA " << id << " NAO EXISTE"
+#define account_created(id) "OK: CONTA " << id << " CADASTRADA"
+#define account_removed(id) "OK: CONTA " << id << " REMOVIDA"
 }  // namespace locale
 
 namespace search_result {
-#define user_not_found;
+
 }
