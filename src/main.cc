55be700697fb3b2c43b9d6b7807ffc20a489ad4d
Brendo <brendo.coder@gmail.com>
2022-10-12 23:45:12 -0300

    Fixed wrong argument type being passed to parse_input

--- src/main.cc
+++ src/main.cc
@@ -9,41 +9,40 @@
 
 using namespace std;
 
-Controller* controller;
+Controller *controller;
 
-void parse_input(istringstream& in) {
-  string word_ = "";
-  in >> word_;
-  utils::lower_string(word_);
-  if (word_ == input_user_register) {
-    int id;
-    in >> id;
-    controller->register_user_by_id(id);
-    return;
-  }
-  return;
+void parse_input(istringstream &in) {
+    string word_;
+    in >> word_;
+    utils::lower_string(word_);
+    if (word_ == input_user_register) {
+        int id;
+        in >> id;
+        controller->register_user_by_id(id);
+        return;
+    }
 }
 
-int main(int argc, char** argv) {
-  // File for inputs
-  string file = argv[1];
+int main(int argc, char **argv) {
+    // File for inputs
+    string file = argv[1];
 
-  ifstream inputs_(file);
-  if (!inputs_.is_open()) {
-    cout << "Failed to load the specified file."
-         << "\n";
-    cout << "Usage: ./run.out <filename>"
-         << "\n";
-    return 1;
-  }
+    ifstream inputs_(file);
+    if (!inputs_.is_open()) {
+        cout << "Failed to load the specified file."
+             << "\n";
+        cout << "Usage: ./run.out <filename>"
+             << "\n";
+        return 1;
+    }
 
-  controller = new Controller();
+    controller = new Controller();
 
-  string line = "";
-  while (getline(inputs_, line)) {
-    istringstream iss(line);
-    parse_input(iss);
-  }
+    string line;
+    while (getline(inputs_, line)) {
+        istringstream iss(line);
+        parse_input(iss);
+    }
 
-  return 0;
+    return 0;
 }
