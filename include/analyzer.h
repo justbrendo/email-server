1254525a30359ae38ec1b6930fcef8c77ba8dcd1
Brendo <brendo.coder@gmail.com>
2022-10-16 12:41:24 -0300

    Changed file formats. Fixed Bug in user removal. Created the analyzer class.

---
+++ include/analyzer.h
@@ -0,0 +1,16 @@
+//
+// Created by yusuf on 16/10/22.
+//
+
+#ifndef EMAIL_SERVER_ANALYZER_H
+#define EMAIL_SERVER_ANALYZER_H
+
+namespace analyzer {
+
+    class analyzer {
+        void performanceTest();
+    };
+
+} // analyzer
+
+#endif //EMAIL_SERVER_ANALYZER_H
---
+++ src/analyzer.cpp
@@ -0,0 +1,13 @@
+//
+// Created by yusuf on 16/10/22.
+//
+
+#include "../include/analyzer.h"
+
+namespace analyzer {
+
+    void analyzer::performanceTest() {
+
+    }
+
+} // analyzer
